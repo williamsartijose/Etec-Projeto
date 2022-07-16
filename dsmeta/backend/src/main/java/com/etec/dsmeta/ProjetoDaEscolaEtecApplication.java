88b1e3f00f30844e193a17a540117aa72d0fb8ca
William sarti josé <william.jose@totvs.com.br>
2022-07-16 20:18:23 -0300

    first commit

---
+++ dsmeta/backend/src/main/java/com/etec/dsmeta/ProjetoDaEscolaEtecApplication.java
@@ -0,0 +1,13 @@
+package com.etec.dsmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDaEscolaEtecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDaEscolaEtecApplication.class, args);
+	}
+
+}
